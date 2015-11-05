44080e17104c7e28750d4b04b4cfd69ca39d0fc6
Emil Kilhage <emilkilhage@gmail.com>
2015-11-05 13:25:31 +0100

    started working on the execute action command

---
+++ src/Command/ActionCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DRI\SugarCRM\Plugin\Command;
+
+use DRI\SugarCRM\Plugin\Path;
+use DRI\SugarCRM\Tests\Bootstrap;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Emil Kilhage
+ */
+class ActionCommand extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('action');
+        $this->addArgument('target', InputArgument::REQUIRED, 'Target sugarcrm path');
+        $this->addArgument('name', InputArgument::REQUIRED, 'Action command');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $config = $this->getConfig();
+        $root = Path::getRootPath();
+
+        $script = $config->get('installdefs.'.$input->getArgument('name'));
+
+        $script = str_replace('<basepath>', $root, $script);
+
+        chdir($input->getArgument('target'));
+
+        Bootstrap::bootSugar();
+
+        var_dump($script);
+    }
+}
